717cbddae5f0c961ca45e40900b344d159dee66e
Bing Xia <aix.bing@gmail.com>
2013-04-21 13:24:43 -0400

    Added ibuffer settings

---
+++ personal/bing-ibuffer.el
@@ -0,0 +1,14 @@
+;;; bing-ibuffer.el --- Personal settings for ibuffer in prelude
+
+;;; Code:
+(prelude-ensure-module-deps '(ibuffer-vc))
+
+(setq ibuffer-show-empty-filter-groups nil)
+(add-hook 'ibuffer-mode-hook
+          (lambda ()
+            (ibuffer-auto-mode 1)
+            (ibuffer-vc-set-filter-groups-by-vc-root)
+            (unless (eq ibuffer-sorting-mode 'alphabetic)
+              (ibuffer-do-sort-by-alphabetic))))
+
+;;; bing-ibuffer.el ends here
