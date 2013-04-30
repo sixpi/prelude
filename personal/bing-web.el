8bcfe0c614c030c47921fc0ed72e0019454a629e
Bing Xia <aix.bing@gmail.com>
2013-04-30 19:05:35 -0400

    Better ibuffer, add web-mode

--- personal/bing-web.el
+++ personal/bing-web.el
@@ -1,9 +1,22 @@
 ;;; bing-web.el --- My configurations for web development modes
 
 ;;; Code:
-(prelude-ensure-module-deps '(php-mode apache-mode))
+(prelude-ensure-module-deps '(php-mode apache-mode web-mode))
 
-(add-to-list 'auto-mode-alist '("\\.php$" . php-mode))
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.jsp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.php$" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.blade.php$" . web-mode))
+
+(setq web-mode-engines-alist
+      '("blade"  . "\\.blade\\."))
+(setq web-mode-indent-style 2)
 
 (setq js-indent-level 2)
 
