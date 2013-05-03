ee8016debc25ea239b82f3af072c6b805087a39b
Bing Xia <aix.bing@gmail.com>
2013-05-03 10:19:31 -0400

    Tinker with web-mode

--- personal/bing-web.el
+++ personal/bing-web.el
@@ -11,12 +11,20 @@
 (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.php$" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.blade.php$" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.php$" . php-mode))
+(add-to-list 'auto-mode-alist '("\\.blade\\.php$" . web-mode))
 
 (setq web-mode-engines-alist
-      '("blade"  . "\\.blade\\."))
-(setq web-mode-indent-style 2)
+      '(("blade"  . "\\.blade\\.php$")))
+
+(defun bing/web-mode-hook ()
+  "Hook for web-mode"
+  (setq web-mode-indent-style 2)
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 4)
+)
+(add-hook 'web-mode-hook 'bing/web-mode-hook)
 
 (setq js-indent-level 2)
 
