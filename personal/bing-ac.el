8e395817af5f18acf0bad0ee1aabc3482a0f8ffe
Bing Xia <aix.bing@gmail.com>
2013-05-17 20:12:08 -0400

    Update autocomplete settings

--- personal/bing-ac.el
+++ personal/bing-ac.el
@@ -20,5 +20,26 @@
 (require 'auto-complete-clang)
 (global-set-key (kbd "C-`") 'ac-complete-clang)
 (setq ac-quick-help-delay 0.3)
+(defun bing/ac-clang-config ()
+  (setq-default ac-sources '(ac-source-abbrev
+                             ac-source-dictionary
+                             ac-source-words-in-same-mode-buffers)))
+(setq ac-clang-flags
+      (mapcar (lambda (item) (concat "-I" item))
+              (split-string
+               "
+ /home/bing/include
+ .
+ /usr/lib/gcc/x86_64-linux-gnu/4.7/include
+ /usr/local/include
+ /usr/lib/gcc/x86_64-linux-gnu/4.7/include-fixed
+ /usr/include/x86_64-linux-gnu
+ /usr/include
+ /usr/include/c++/4.7
+ /usr/include/x86_64-linux-gnu/c++/4.7/.
+ /usr/include/c++/4.7/backward
+"
+               )))
+(bing/ac-clang-config)
 
 ;;; bing-ac.el ends here
