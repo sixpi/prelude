1a719504f8edcdc48102d61dfccaf1cabd329069
Bing Xia <aix.bing@gmail.com>
2013-07-09 14:21:17 -0400

    Fix personal snippet directory location

--- personal/bing-ac.el
+++ personal/bing-ac.el
@@ -5,7 +5,7 @@
 
 ;; Make sure yasnippet is present
 (require 'yasnippet)
-(yas-global-mode 1) 
+(yas-global-mode 1)
 
 (require 'auto-complete-config)
 (add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
