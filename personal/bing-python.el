262df9818ce0616b6ac93ffb32c632d2581013c3
Bing Xia <aix.bing@gmail.com>
2013-07-24 14:36:51 -0400

    Add virtualenv

--- personal/bing-python.el
+++ personal/bing-python.el
@@ -4,9 +4,12 @@
 ;(require 'ipython)
 ;(require 'cython-mode)
 
-(prelude-ensure-module-deps '(jedi))
+(prelude-ensure-module-deps '(jedi virtualenv))
 
 (setq jedi:setup-keys t)
+(setq jedi:complete-on-dot t)
 (add-hook 'python-mode-hook 'jedi:setup)
+
+(require 'virtualenv)
 
 ;;; bing-python.el ends here
